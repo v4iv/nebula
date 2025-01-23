78c27f4c9c581ac68fbd044041afa35d91308c46
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-23 14:34:59 +0530

    feat: reading time plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
 import react from "@astrojs/react"
 import tailwind from "@astrojs/tailwind"
+
+import { readingTime } from "./src/plugins/remark/reading-time"
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.URL || "http://localhost:4321",
   integrations: [react(), tailwind({ applyBaseStyles: false })],
   markdown: {
+    remarkPlugins: [readingTime],
     shikiConfig: {
       theme: "houston",
     },
