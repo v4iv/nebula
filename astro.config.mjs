30ac117f9646b8e62b413034e71106ddc9ebc2ad
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-26 02:30:21 +0530

    feat: sitemap.xml & robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,19 @@ import tailwind from "@astrojs/tailwind"
 
 import { readingTime } from "./src/plugins/remark/reading-time"
 
+import sitemap from "@astrojs/sitemap"
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.URL || "http://localhost:4321",
-  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    react(),
+    tailwind({ applyBaseStyles: false }),
+    sitemap({
+      filter: (page) =>
+        page !== `${process.env.URL || "http://localhost:4321"}/admin/`,
+    }),
+  ],
   markdown: {
     remarkPlugins: [readingTime],
     shikiConfig: {
@@ -18,5 +27,8 @@ export default defineConfig({
   experimental: {
     responsiveImages: true,
     contentIntellisense: true,
+  },
+  prefetch: {
+    defaultStrategy: "viewport",
   },
 })
