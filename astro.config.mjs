fb18bb7efce86a66e111530b7308c50ad959a9dd
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-27 12:28:21 +0530

    feat: sitemap filter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 
 import { readingTime } from "./src/plugins/remark/reading-time"
 
-import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +14,7 @@ export default defineConfig({
     react(),
     tailwind({ applyBaseStyles: false }),
     sitemap({
-      filter: (page) =>
-        page !== `${process.env.URL || "http://localhost:4321"}/admin/`,
+      filter: (page) => !page.includes(`/admin/`) && !page.includes(`/tags/`),
     }),
   ],
   markdown: {
